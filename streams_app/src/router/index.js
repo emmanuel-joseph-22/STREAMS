46481d768847b9d60c6dfe5f449111f7450efa6b
emmanuel-joseph-22 <21-05559@g.batstate-u.edu.ph>
2024-03-10 09:49:48 +0800

    changed project setup (medyo nagulo ko code ni yvan)

--- streams_app/src/router/index.js
+++ streams_app/src/router/index.js
@@ -1,36 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import SignUp from '../views/authentication/LogIn.vue'
-import HomeView from '../views/HomeView.vue'
-import LogIn from '../views/authentication/LogIn.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import LogIn from '../views/authentication/LogIn.vue';
 
-const router =createRouter({
-  history: createWebHistory(),
-  base: process.env.BASE_URL,
-  routes : [
+const routes = [
     {
       path: '/',
       redirect: '/login'
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     },
     {
       path: '/login',
       name: 'login',
       component: LogIn
     }
-  ]
-})
+]
 
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+  })
 
 export default router
