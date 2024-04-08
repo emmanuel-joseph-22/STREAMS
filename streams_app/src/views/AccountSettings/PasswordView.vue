f428b7d36cd2dfb0ccd94162caecf794a669ac16
apriljavier <21-09454@g.batstate-u.edu.ph>
2024-04-08 08:20:06 +0800

    acct setting tw

--- streams_app/src/views/AccountSettings/PasswordView.vue
+++ streams_app/src/views/AccountSettings/PasswordView.vue
@@ -1,159 +1,48 @@
 <template>
     <home-page>
-        <div class="default">
-        <div class="cont">
-        <div class="change">
-            <label for="input-change" class="change-label">Old Password</label>
-            <input autofocus id="input-change" type="text" name="change" required placeholder="Old Password" class="change-input"/>
-        </div>
-        <div class="change-1">
-            <label for="input-change-1" class="change-1-label">New Password</label>
-            <input autofocus id="input-change-1" type="text" name="change-1" required placeholder="New Password" class="change-1-input"/>
-        </div>
-        <div class="change-2">
-            <label for="input-change-2" class="change-2-label">Retype New Password</label>
-            <input autofocus id="input-change-2" type="text" name="change-2" required placeholder="Retype New Password" class="change-2-input"/>
-        </div>
-        <router-link to="/settings">
-            <div class="body">
-                    <a href="#">
-                        <span>CONFIRM</span>
-                        <div class="wave"></div>
-                    </a>
+        <div class="default flex flex-col items-center justify-center mt-20">
+            <div class="cont bg-blue-900 rounded-lg lg:w-1/3 xl:w-1/4 h-60vh flex flex-col items-center justify-center" style="background-color: #042334;">
+                <div class="p-4 w-full flex flex-col items-center">
+                    <div class="change flex flex-col items-start lg:w-3/4 mt-10">
+                        <label for="input-change" class="change-label font-bold text-white">Current Password</label>
+                        <input v-model="currentPassword" id="input-change" name="change" required placeholder="Old Password" class="change-input w-full lg:w-3/2 px-4 py-2 border-2 border-blue-500 rounded-md text-base text-blue-900 focus:outline-none">
+                    </div>
+                    <div class="change-1 flex flex-col items-start lg:w-3/4 mt-10">
+                        <label for="input-change-1" class="change-1-label font-bold text-white">New Password</label>
+                        <input autofocus id="input-change-1" type="password" name="change-1" required placeholder="New Password" class="change-1-input w-full lg:w-3/2 px-4 py-2 border-2 border-blue-500 rounded-md text-base text-blue-900 focus:outline-none">
+                    </div>
+                    <div class="change-1 flex flex-col items-start lg:w-3/4 mt-10">
+                        <label for="input-change-2" class="change-1-label font-bold text-white">Re-type Password</label>
+                        <input autofocus id="input-change-2" type="password" name="change-2" required placeholder="Re-type Password" class="change-1-input w-full lg:w-3/2 px-4 py-2 border-2 border-blue-500 rounded-md text-base text-blue-900 focus:outline-none">
+                    </div>
+                    <router-link to="/settings">
+                        <div class="body mt-4 p-5">
+                            <a href="#">
+                                <span>Confirm</span>
+                                <div class="wave"></div>
+                            </a>
+                        </div>
+                    </router-link>
                 </div>
-        </router-link>
+            </div>
         </div>
-    </div>
     </home-page>
 </template>
 
 <script>
 import AccountPageView from './AccountPageView.vue';
 
-    export default {
-        components: {
-            'home-page': AccountPageView,
-        },
-        data(){
+export default {
+    components: {
+        'home-page': AccountPageView,
+    },
+    data() {
+        return {
+            currentPassword: 'waterh2o',
+        };
+    },
+    mounted() {
 
-        },
-        mounted(){
-        
-        }
-    }
+    },
+};
 </script>
-
-<style scoped>
-    .default{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        cursor: pointer;
-    }
-
-    .cont{
-        background-color: #042334;
-        border-radius: 20px;
-        width: 50%;
-        height: 80vh;
-        margin-top: 55px;
-        margin-left: 10px;
-    }
-
-    .change,
-    .change-1,
-    .change-2 {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 60%;
-        margin-top: 40px;
-        margin-left: 18%;
-    }
-
-    .change-label,
-    .change-1-label {
-        margin-right: 60%;
-        font-weight: bold;
-        color: #FFFFFF;
-    }
-
-    .change-2-label{
-        margin-right: 45%;
-        font-weight: bold;
-        color: #FFFFFF;
-    }
-
-    .change-input,
-    .change-1-input,
-    .change-2-input { 
-        width: 90%;
-        flex: 1;
-        padding: 10px;
-        border: 2px solid #5F88BF;
-        border-radius: 4px;
-        font-size: 16px;
-        color: #3B5271;
-        box-sizing: border-box;
-        outline: none;
-    }
-
-
-    .body{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-height: 25vh;
-    }
-    .body a{
-        position: relative;
-        display: block;
-        padding: 10px 30px;
-        border: 2px solid #00aeff;
-        border-radius: 50px;
-        text-transform: uppercase;
-        font-size: 18px;
-        letter-spacing: 4px;
-        color: #00aeff;
-        text-decoration: none;
-        overflow: hidden
-    }
-    .body a span{
-        position: relative;
-        z-index: 1;
-        transition: 1s;
-    }
-    .body a .wave{
-        position: absolute;
-        top: calc(100% + 22px);
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: #00aeff;
-        transition: 1s;
-    }
-    .body a:hover .wave{
-        top: 0;
-    }
-    .body a .wave::before{
-        content: '';
-        position: absolute;
-        top: -22px;
-        left: 0;
-        width: 100%;
-        height: 22px;
-        background-image: 'wave.png';
-        animation: animate 0.5s linear infinite;
-    }
-    @keyframes animate{
-            0%
-        {
-            background-position-x: 0;
-            background-position-x: -22px;
-        }
-        100%
-        {
-            background-position-x: 118px;
-        }
-    }
-</style>
--- streams_app/src/views/AccountSettings/SettingsView.vue
+++ streams_app/src/views/AccountSettings/SettingsView.vue
@@ -1,50 +1,19 @@
 <template>
     <home-page>
-        <div class="account-cont">
-            <div class="cont">
-            <img src="user.png" alt="User" class="user-img">
-            <div class="settings">
-                <h1 class="mr-4">AQUATECH</h1>
-                <h2>aquatech@gmail.com</h2>
-                <!--<div class="def-name">
-                    <label for="def-name" class="def-name-label">Default Name</label>
-                    <input autofocus id="def-name" type="text" required placeholder="Default Name" class="def-name-input"/>
-                </div>
-                
-                <router-link to="/password">-->
-                <!--<div class="password">
-                    <label for="password" class="password-label">Change Password</label>
-                    <div class="password-input-cont">
-                        <input autofocus id="password" type="text" required placeholder="Password" class="password-input"/>
-                        <router-link to="/password">
-                        <img src="next.png" alt="next-icon">
-                        </router-link>
-                    </div>   
-                </div>-->
-                <!--</router-link>-->
-                <div class="default">
-                    <h3>Default Name</h3>
-                    <div class="def-icon">
-                        <router-link to="/defname"><img src="next.png" alt=""></router-link>
+        <div class="account-cont flex flex-col items-center justify-center mt-20">
+            <div class="cont bg-blue-900 rounded-lg lg:w-1/3 xl:w-1/2 h-60vh flex flex-col items-center justify-center" style="background-color: #042334;">
+                <div class="p-4 w-full flex flex-col items-center mb-20">
+                    <div class="change flex flex-col items-center lg:w-3/4 mt-10">
+                        <label for="input-change" class="change-label font-bold text-white text-4xl">{{ defaultName }}</label>
+                        <label for="input-change" class="change-label font-bold text-white">{{ emailAd }}</label>
                     </div>
+                    <router-link to="/defname" class="change-box flex flex-col items-center border-2 border-blue-500 lg:w-3/4 mt-10 rounded-lg bg-white p-4">
+                        <label for="input-change" class="change-label font-bold text-black">Change Default Name</label>
+                    </router-link>
+                    <router-link to="/password" class="change-box flex flex-col items-center border-2 border-blue-500 lg:w-3/4 mt-10 rounded-lg bg-white p-4">
+                        <label for="input-change" class="change-label font-bold text-black">Change Password</label>
+                    </router-link>
                 </div>
-
-                <div class="password">
-                    <h4>Change Password</h4>
-                    <div class="pass-icon">
-                        <router-link to="/password"><img src="next.png" alt=""></router-link>
-                    </div>
-                </div>
-
-                <router-link to="/home">
-                    <div class="body">
-                        <a href="#">
-                            <span>SUBMIT</span>
-                            <div class="wave"></div>
-                        </a>
-                    </div>
-                </router-link>
-            </div>
             </div>
         </div>
     </home-page>
@@ -57,148 +26,14 @@ export default {
     components: {
         'home-page': AccountPageView,
     },
-    data(){
-
+    data() {
+        return {
+            defaultName: 'Aquatech',
+            emailAd: 'aquatech@gmail.com'
+        };
     },
-    mounted(){
+    mounted() {
       
     }
 }
 </script>
-
-<style scoped>
-    .account-cont{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        cursor: pointer;
-    }
-
-    .account-cont img{
-        width: 130px;
-        height: 130px;
-        margin: 15px;
-        margin-left: 40%;
-    }
-
-    .cont{
-        background-color: #042334;
-        border-radius: 20px;
-        width: 50%;
-        height: 95vh;
-        margin-top: 15px;
-        margin-left: 10px;
-    }
-
-    .settings{
-        flex: 1;
-        padding: 8px;
-        margin-top: 10px;
-        margin-left: 8px;
-        width: 100%;
-    }
-
-    .settings h1{
-        font-size: 2rem;
-        font-weight: bold;
-        font-family: 'Times New Roman', Times, serif;
-        color: #FFFFFF;
-    }
-
-    .settings h2{
-        color: #4E636F;
-        font-weight: bold;
-    }
-
-/*check*/
-    .default h3,
-    .password h4{
-        color: #0E5E7B;
-        font-size: 16px;
-        font-weight: bold;
-        margin-top: 10px;
-        margin-right: 60%;
-        opacity: 1;
-    }
-/*check*/
-    .default,
-    .password{
-        width: 60%;
-        height: 45px;
-        box-sizing: border-box;
-        border: 2px solid #5F88BF;
-        border-radius: 3px;
-        margin-top: 40px;
-        margin-left: 20%;
-        background-color: #FFFFFF;
-        align-items: center;
-    }   
-/*check*/
-    .def-icon img,
-    .pass-icon img{
-        width: 30px;
-        height: 30px;
-        margin: 10px;
-        margin-top: -25px;
-        margin-left: 26vw;
-    }
-
-    .body{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-height: 25vh;
-    }
-    .body a{
-        position: relative;
-        display: block;
-        padding: 10px 30px;
-        border: 2px solid #00aeff;
-        border-radius: 50px;
-        text-transform: uppercase;
-        font-size: 18px;
-        letter-spacing: 4px;
-        color: #00aeff;
-        text-decoration: none;
-        overflow: hidden
-    }
-    .body a span{
-        position: relative;
-        z-index: 1;
-        transition: 1s;
-    }
-    .body a .wave{
-        position: absolute;
-        top: calc(100% + 22px);
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: #00aeff;
-        transition: 1s;
-    }
-    .body a:hover .wave{
-        top: 0;
-    }
-    .body a .wave::before{
-        content: '';
-        position: absolute;
-        top: -22px;
-        left: 0;
-        width: 100%;
-        height: 22px;
-        /*dl nyo yung wave.png na asa public then paltan nyo na lang areng url nung location nung wave sa device nyo*/
-        background-image: 'wave.png';
-        animation: animate 0.5s linear infinite;
-    }
-    @keyframes animate{
-            0%
-        {
-            background-position-x: 0;
-            background-position-x: -22px;
-        }
-        100%
-        {
-            background-position-x: 118px;
-        }
-    }
-</style>
