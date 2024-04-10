f0c86e552d4c1d3b367e81f42214c244439e5a7a
Reyes-CassiusWayne <142638408+Reyes-CassiusWayne@users.noreply.github.com>
2024-04-10 23:40:56 +0800

    napapakita na ung map pero aayusin pa ung zoom, move, at tilt

--- streams_app/src/views/dashboard/CampusMapView.vue
+++ streams_app/src/views/dashboard/CampusMapView.vue
@@ -1,16 +1,11 @@
 <template>
     <home-page>
         <main-content>
-            <iframe
-                width="600"
-                height="450"
-                style="border:0"
-                loading="lazy"
-                allowfullscreen
-                referrerpolicy="no-referrer-when-downgrade"
-                src="https://www.google.com/maps/embed/v1/place?key=API_KEY
-                &q=Space+Needle,Seattle+WA">
-            </iframe>
+          <div class="map-wrap">
+            <a href="https://www.maptiler.com" class="watermark"><img
+            src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"/></a>
+            <div class="map" ref="mapContainer"></div>
+          </div>
         </main-content>
     </home-page>
 </template>
@@ -18,32 +13,71 @@
 <script>
 import HomePageView from './HomePageView.vue';
 import dashboard_content from '../../components/dashboard_content.vue'
-import { Capacitor } from '@capacitor/core'; // Import Capacitor from Capacitor
+import { Map } from 'maplibre-gl';
+import { shallowRef, onUnmounted, markRaw, watch } from 'vue';
+
 
 export default {
-    components: {
-        'home-page': HomePageView,
-        'main-content': dashboard_content
-    },
-    mounted() {
-        document.addEventListener('backbutton', this.handleBackButton);
-    },
-    unmounted() {
-        document.removeEventListener('backbutton', this.handleBackButton);
-    },
-    methods: {
-        handleBackButton() {
-        if (Capacitor.isNative) {
-            if (this.$router.currentRoute.path !== '/') {
-            this.$router.go(-1); // Navigate back if not on the home page
-            } else {
-            if (window.confirm('Do you want to exit the app?')) {
-                navigator.app.exitApp(); // Exit the app if on the home page
-            }
-            }
+  components: {
+    'home-page': HomePageView,
+    'main-content': dashboard_content
+  },
+  name: "MapComponent",
+  setup() {
+    const mapContainer = shallowRef(null);
+    const map = shallowRef(null);
+
+
+    // Watch for changes to mapContainer.value
+    watch(
+      () => mapContainer.value,
+      (newValue, oldValue) => {
+        if (newValue && !oldValue) {
+          const apiKey = '3Giyb8izMH7QvlPNRm0I';
+          const initialState = { lng: 121.074490, lat: 13.784249, zoom: 18 };
+
+          map.value = markRaw(
+            new Map({
+              container: newValue,
+              style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`,
+              center: [initialState.lng, initialState.lat],
+              zoom: initialState.zoom
+            })
+          );
         }
-        }
-    }
-}
+      }
+    );
+    onUnmounted(() => {
+      map.value?.remove();
+    });
+
+    return {
+      map,
+      mapContainer
+    };
+  }
+};
 </script>
 
+<style scoped>
+@import '~maplibre-gl/dist/maplibre-gl.css';
+
+.map-wrap {
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 77px); /* calculate height of the screen minus the heading */
+}
+
+.map {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.watermark {
+  position: absolute;
+  left: 10px;
+  bottom: 10px;
+  z-index: 999;
+}
+</style>
